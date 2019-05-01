71a4607b11f140100da6ac019b125c0393c14cb0
Calvin Fronda <cfronda209@gmail.com>
2019-04-30 21:16:48 -0700

    added smooth scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -173,7 +173,16 @@ $(".dislike").one("click", function (event, id) {
   })
 
 });
+$(document).ready(function () {
 
+
+  //smooth scrolling
+  $("a[href='#top']").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+  });
+
+});
 
 
 
